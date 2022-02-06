6eee23650b10aaae69fc0168d0ed17125d8ca845
DanXaPd <92036667+DanXaPd@users.noreply.github.com>
2022-02-06 21:36:09 +0200

    tasks_for

---
+++ main.js
@@ -0,0 +1,42 @@
+// Number_1
+/*let y
+for (let x = 1; x <= 10; x++) {
+    y = x**2;
+    console.log(y);
+}*/
+
+//Number_2
+/*let n = +prompt("Введите конечное число")
+let sum = 0
+for (let s = 1; s <= n; s++) {
+    sum=sum+s;
+}
+console.log(sum);*/
+
+//Number_3
+/*let vklad = 10000;
+let dohod = 1.10;
+for (let year = 1; year <= 5; year++) {
+    vklad = vklad*dohod
+}
+console.log(vklad)*/
+
+//Number_4
+/*let a = +prompt("Введите количество слагаемых")
+let sum = 0
+let b = 1/2
+for (let c = 1; c <= a; c++) {
+    sum+=b 
+    b/=2
+}
+console.log(sum)*/
+
+//Number_5
+/*alert("Lets go?")
+for (let a = 1; a <= 10; a++) {
+    for (let b = 1; b <=10; b++) {
+        console.log(`${a} * ${b} = ${a*b}`)
+    }
+    console.log("--------------------")
+}*/
+
